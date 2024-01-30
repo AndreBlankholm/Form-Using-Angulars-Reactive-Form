74b9dab8c0ee5c628df87581a9fad13d4058cfb0
AndreBlankholm <ablankholm@gmail.com>
2024-01-29 18:37:27 -0600

    added hobbies and trying to add the array of hobbies and display them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { variationPlacements } from '@popperjs/core';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +16,25 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl('Male')
+      "userData": new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      
+      'gender': new FormControl('Male'),
+      'hobbies': new FormArray([])
     });
   }
 
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    console.log("Clicked");
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
 
 
 }
